5eb8ffdf012a5bcd3d3f381046e560fc44984b03
Sotiris Tsartsaris <tsartsaris@gmail.com>
2018-11-18 15:21:35 +0200

    Create VisibleComponent.cs
    initial commit

---
+++ Scripts/SharedDataComponents/VisibleComponent.cs
@@ -0,0 +1,18 @@
+using System;
+using Unity.Entities;
+
+/// <summary>
+/// shared component data used by EntityManager
+/// for filtering over groups of pooled entities
+/// 0 = Not Visible 1 = Visible
+/// values are set automatically on spawn despawn
+/// </summary>
+
+[Serializable]
+public struct Visible : ISharedComponentData {
+    public int Value;
+}
+
+[UnityEngine.DisallowMultipleComponent]
+public class VisibleComponent : SharedComponentDataWrapper<Visible> {
+}
